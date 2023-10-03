e92773b299e297352b7e303b25444a1224b9f9b7
surialocal <surivig18@gmail.com>
2023-10-03 22:39:34 +0800

    some parallel execution code

--- src/main/java/com/suria/sample/controller/SampleController.java
+++ src/main/java/com/suria/sample/controller/SampleController.java
@@ -4,12 +4,34 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
 @RestController
 public class SampleController {
 
 
     @GetMapping
-    public String  getJavaVersion(){
+    public String  getJavaVersion() throws InterruptedException {
+         ExecutorService executorService = Executors.newSingleThreadExecutor();
+         var list = new ArrayList();
+         for(int i =0 ; i < 100000; i++)
+            list.add(new RunnableTask());
+        executorService.invokeAll(list);
         return Runtime.version().toString();
+    }
+}
+
+class RunnableTask implements Callable {
+
+    @Override
+    public Object call() throws Exception {
+        ConcurrentHashMap concurrentHashMap = new ConcurrentHashMap();
+        concurrentHashMap.put("Key", "Value");
+        return  concurrentHashMap;
     }
 }
